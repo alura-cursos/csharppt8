3246aeda206312ba9d43f1fdd0768878c382a52d
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-07-16 23:25:26 +0000

    Projeto da aula 5 - OrderBy e expressões Lambda

---
+++ ByteBank.Modelos/GerenciadorBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Modelos.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
