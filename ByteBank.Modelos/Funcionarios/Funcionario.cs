b98f3a9436356c417810e696f32d3b0933e4c726
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-07-16 23:25:14 +0000

    Projeto da aula 4 - IComparable e IComparer

---
+++ ByteBank.Modelos/Funcionarios/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos.Funcionarios
+{
+    public abstract class Funcionario
+    {
+        public static int TotalDeFuncionarios { get; private set; }
+
+        public string Nome { get; set; }
+
+        public string CPF { get; private set; }
+        
+        public double Salario { get; protected set; }
+
+        public Funcionario(double salario, string cpf)
+        {
+            Console.WriteLine("Criando FUNCIONARIO");
+
+            CPF = cpf;
+            Salario = salario;
+
+            TotalDeFuncionarios++;
+        }
+
+        public abstract void AumentarSalario();
+
+        internal protected abstract double GetBonificacao();
+    }
+}
---
+++ ByteBank.Modelos/Funcionarios/GerenteDeConta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos.Funcionarios
+{
+    public class GerenteDeConta : FuncionarioAutenticavel
+    {
+        public GerenteDeConta(string cpf) : base(4000, cpf)
+        {
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.05;
+        }
+
+        internal protected override double GetBonificacao()
+        {
+            return Salario * 0.25;
+        }
+    }
+}
